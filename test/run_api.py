ed20d615c1cc1bd5ef5c00884647ab583b6b4c94
Kayla Rossi <kayla.m.rossi22@gmail.com>
2026-03-20 17:20:30 -0400

    model refinement and feature additions

--- test/run_api.py
+++ test/run_api.py
@@ -1,15 +1,18 @@
+import json
 import requests
-import pandas as pd
 
-BASE_URL = "http://54.227.34.176:5000"
 
-with open ("test/simple_post_data_subset.json", 'r') as f:
-    post_data = pd.read_json(f)
+BASE_URL = "http://54.227.34.176:5000"  #update based on EC2 public url
+DATA_PATH = "test/simple_post_data_subset.json"
+END_POINT = "/predict/simple"
 
+
+with open (DATA_PATH, 'r') as f:
+    post_data = json.load(f)
 
 response = requests.post(
-    f"{BASE_URL}/predict", 
-    json=post_data.to_dict(orient='records')
+    f"{BASE_URL}{END_POINT}", 
+    json=post_data
 )
 
 
